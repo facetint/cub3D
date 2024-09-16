473d3f74a7890d13f890fbff5d586e80a9ae4fd1
Fatmanur Cetintas <facetint@k1m23s05.42kocaeli.com.tr>
2024-09-16 12:04:49 +0300

    refactor: add functions to correctly release everything opened with mlx

---
+++ srcs/control/destroy.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destroy.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 20:13:01 by facetint          #+#    #+#             */
+/*   Updated: 2024/09/16 12:03:09 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minilibx/mlx.h"
+#include "../../includes/config.h"
+#include "../../safe_alloc/allocator.h"
+#include <stdlib.h>
+
+t_player	*get_player(void)
+{
+	static t_player	player = {0};
+
+	return (&player);
+}
+
+void	free_all(t_player *player)
+{
+	if (player->map.texture.east.img)
+		mlx_destroy_image(player->mlx.mlx, player->map.texture.east.img);
+	if (player->map.texture.north.img)
+		mlx_destroy_image(player->mlx.mlx, player->map.texture.north.img);
+	if (player->map.texture.south.img)
+		mlx_destroy_image(player->mlx.mlx, player->map.texture.south.img);
+	if (player->map.texture.west.img)
+		mlx_destroy_image(player->mlx.mlx, player->map.texture.west.img);
+	free_memory();
+}
+
+void	handle_abort(void)
+{
+	free_all(get_player());
+}
+
+void	safe_exit(void)
+{
+	handle_abort();
+	exit(0);
+}
